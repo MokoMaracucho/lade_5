001b57f806ad0cfed49c4413c45d50690c21563a
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-20 21:16:11 +0100

    LadeApplicatio.java : SpringApplicationBuilder

--- src/main/java/com/oc/moko/lade/LadeApplication.java
+++ src/main/java/com/oc/moko/lade/LadeApplication.java
@@ -2,12 +2,18 @@ package com.oc.moko.lade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LadeApplication {
+public class LadeApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LadeApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LadeApplication.class, args);
 	}
-
 }
