6c27a985352b3f42af47c5f5afae707c0b2daa5a
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-20 20:46:35 +0100

    Spring Initializr : Spring Web - Spring Data JPA - MySQL Driver - Spring Boot DevTools

---
+++ src/main/java/com/oc/moko/lade/LadeApplication.java
@@ -0,0 +1,13 @@
+package com.oc.moko.lade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oc/moko/lade/LadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.moko.lade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
